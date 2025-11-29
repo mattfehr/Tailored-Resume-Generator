482062fd91e6337bc069fd5db5d5b22e35d34126
Matthew Fehr <mattfehr2004@gmail.com>
2025-11-28 18:44:22 -0800

    add auth config

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,3 +8,6 @@ class Config:
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
     ENV = os.getenv("ENV", "development")
     GEMINI_MODEL = os.getenv("GEMINI_MODEL", "models/gemini-2.5-pro")
+
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
